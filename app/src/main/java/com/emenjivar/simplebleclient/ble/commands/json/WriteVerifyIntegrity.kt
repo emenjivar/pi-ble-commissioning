7666b4d11669773fc32ba4e456321354c9ebf557
emenjivar <carlos.itca@gmail.com>
2026-08-23 09:20:37 -0600

    feat: Add writeVerifyIntegrity command

---
+++ app/src/main/java/com/emenjivar/simplebleclient/ble/commands/json/WriteVerifyIntegrity.kt
@@ -0,0 +1,24 @@
+package com.emenjivar.simplebleclient.ble.commands.json
+
+import com.emenjivar.simplebleclient.ble.commands.BleCommand
+import com.emenjivar.simplebleclient.ble.commands.jsonServiceUUID
+import com.emenjivar.simplebleclient.ble.commands.verifyIntegrityUUID
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+
+/**
+ * Writes a CRC32 code to the GATT server
+ */
+object WriteVerifyIntegrity: BleCommand.Write<Long>(
+    service = jsonServiceUUID,
+    characteristic = verifyIntegrityUUID
+) {
+    override fun encode(value: Long): ByteArray {
+        val bytes = ByteBuffer.allocate(4)
+            .order(ByteOrder.LITTLE_ENDIAN)
+            .putInt(value.toInt())
+            .array()
+
+        return bytes
+    }
+}
